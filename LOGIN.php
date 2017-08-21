780161c777c5f20608b2230ba974d8f989a1a356
markitus1996 <31216165+markitus1996@users.noreply.github.com>
2017-08-21 19:35:39 +0200

    -
    LOGIN

---
+++ LOGIN.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE HTML>
+<html manifest="" lang="es-ES">
+<head>
+    <meta charset="UTF-8">
+    <title>CRM Postventa</title>
+    </head>
+<body>
+<div align="center">
+<img src="img/Logo%20AVI.png">
+</div>
+<div align = "center">
+<H4>Username</H4>
+<input class="input">
+<H4>Password</H4>
+<input class="input" type="password">
+</div>
+<div align="center">
+<img src="img/Logo%20AVI.png">
+</div>
+</body>
+</html>
