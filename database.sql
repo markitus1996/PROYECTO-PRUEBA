02299c5de30069bb2e49d29de0153f96cc0d4379
markitus1996 <31216165+markitus1996@users.noreply.github.com>
2017-08-21 22:01:09 +0200

    CREANDO PAGINA INDEX

--- database.sql
+++ database.sql
@@ -5,8 +5,10 @@ CREATE DATABASE IF NOT EXISTS Proyecto CHARSET utf8 COLLATE utf8_spanish2_ci;
 USE Proyecto;
 
 CREATE TABLE usuarios (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	nombre VARCHAR (255) NOT NULL,
 	email VARCHAR (255) NOT NULL,
     contrasenya VARCHAR (45) NOT NULL
 );
 
+INSERT INTO  usuarios (nombre, email , contrasenya) VALUES ('FFR','123@frm.com','1234');
